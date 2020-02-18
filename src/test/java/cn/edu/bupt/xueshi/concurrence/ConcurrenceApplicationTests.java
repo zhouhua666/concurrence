ba57114345e2a14e57d40df2e9462ba7997a446f
zhouhua666 <1594107061@qq.com>
2020-02-18 17:50:02 +0800

    init

---
+++ src/test/java/cn/edu/bupt/xueshi/concurrence/ConcurrenceApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.bupt.xueshi.concurrence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
